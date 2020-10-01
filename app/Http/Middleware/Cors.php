b07b593399622f4a019e39da7a7441ae5c30c5d7
Alonso Bravo <abravo@movilform.com>
2020-09-30 23:20:44 -0300

    correcion cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,16 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
+        $domain = parse_url($_SERVER['HTTP_REFERER']);
+        $host = "*";
+
+        if (isset($domain['host'])) {
+            $host = $domain['host'];
+        }
+
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Origin', $host);
         $response->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application, Origin, X-Auth-Token');
         $response->header('Access-Control-Allow-Credentials', 'true');
