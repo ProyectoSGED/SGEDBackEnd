d3d528ecc95b78687ac406eeab1dfa426622168f
Alonso Bravo <abravo@movilform.com>
2020-09-30 23:16:15 -0300

    correcion cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,19 +15,13 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        /*$response = $next($request);
+        $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application, Origin, X-Auth-Token');
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application, Origin, X-Auth-Token');
+        $response->header('Access-Control-Allow-Credentials', 'true');
 
-        return $response;*/
-
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://166.110.136.7:9000')
-            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application, Origin, X-Auth-Token')
-            ->header('Access-Control-Allow-Credentials', 'true');
+        return $response;
     }
 }
