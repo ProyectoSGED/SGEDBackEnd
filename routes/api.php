e65b1dbd24327abcb4a317321cb94e635de23717
Alonso Bravo <abravo@movilform.com>
2020-10-01 21:20:13 -0300

    correcion cors

--- routes/api.php
+++ routes/api.php
@@ -19,27 +19,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/users/list', 'UserController@index');
-Route::get('/users/get', 'UserController@getUserById');
-Route::post('/users/new', 'UserController@store');
-Route::put('/users/update', 'UserController@update');
-Route::delete('/users/deactivate', 'UserController@deactivateUser');
+Route::group(['middleware' => ['cors']], function () {
+    Route::get('/users/list', 'UserController@index');
+    Route::get('/users/get', 'UserController@getUserById');
+    Route::post('/users/new', 'UserController@store');
+    Route::put('/users/update', 'UserController@update');
+    Route::delete('/users/deactivate', 'UserController@deactivateUser');
 
-Route::get('/profiles', 'ProfileController@index');
+    Route::get('/profiles', 'ProfileController@index');
 
-Route::put('/shapes/update', "ShapeController@update");
-Route::delete('/shapes/delete', "ShapeController@destroy");
-Route::get('/shapes/list/all', "ShapeController@index");
-Route::post('/shapes/new', "ShapeController@store");
-Route::get('/shapes/file', 'ShapeController@downloadShape');
-Route::get('/shapes/get', "ShapeController@getShapeById");
-Route::get('/shapes/list', 'ShapeController@shapesByCategory');
-Route::get('/shapes/categories', 'CategoriaShapeController@index');
-Route::get('/shapes/search', 'ShapeController@findShapeByQuery');
-Route::get('/shapes/categories/all', 'ShapeController@getShapesCategories');
+    Route::put('/shapes/update', "ShapeController@update");
+    Route::delete('/shapes/delete', "ShapeController@destroy");
+    Route::get('/shapes/list/all', "ShapeController@index");
+    Route::post('/shapes/new', "ShapeController@store");
+    Route::get('/shapes/file', 'ShapeController@downloadShape');
+    Route::get('/shapes/get', "ShapeController@getShapeById");
+    Route::get('/shapes/list', 'ShapeController@shapesByCategory');
+    Route::get('/shapes/categories', 'CategoriaShapeController@index');
+    Route::get('/shapes/search', 'ShapeController@findShapeByQuery');
+    Route::get('/shapes/categories/all', 'ShapeController@getShapesCategories');
 
-Route::post('/contact', "ContactController@sendContactMessage");
-
+    Route::post('/contact', "ContactController@sendContactMessage");
+});
 
 
 Route::middleware(['api'])->group(function ($router) {
